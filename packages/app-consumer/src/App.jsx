b757ef17d4efbd54e9dc07f37ef989a34eb22ad9
Thiago Sousa <thiago.sousa@paylivre.com>
2022-04-18 14:59:15 -0300

    correção5

--- packages/app-consumer/src/App.jsx
+++ packages/app-consumer/src/App.jsx
@@ -1,13 +1,13 @@
 import './styles/global.scss';
-// import { ButtonIcon } from "@paylivre/icons";
-// import { Button } from "@paylivre/components";
+import { ButtonIcon } from "@paylivre/icons";
+import { Button } from "@paylivre/components";
 
 export function App(){
   return (
     <div>
       <h1>Hello Paylivre</h1>
-      {/* <Button /> */}
-      {/* <ButtonIcon /> */}
+      <Button />
+      <ButtonIcon />
     </div>
   )
 }
